a75430833ea36960c88178ad07f84e9292a8b59f
Camron Schwoegler <camron@metafour.org>
2013-10-02 20:16:00 -0600

    Initial Commit

---
+++ HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Camron Schwoegler on 10/2/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
